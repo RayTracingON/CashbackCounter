e9d830c1e26956e98601247056bf354d2892a2a1
Junhao Huang <jh846@students.waikato.ac.nz>
2025-11-23 20:05:45 +0800

    Create database

---
+++ CashbackCounter/AddTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTransactionView.swift
+//  CashbackCounter
+//
+//  Created by Junhao Huang on 11/23/25.
+//
+
+import SwiftUI
+
+struct AddTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTransactionView()
+}
