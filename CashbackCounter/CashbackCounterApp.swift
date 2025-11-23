110e8ac37572e1784b9e5418cafce9afc72786c7
Junhao Huang <jh846@students.waikato.ac.nz>
2025-11-23 14:43:51 +0800

    Initial Commit

---
+++ CashbackCounter/CashbackCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashbackCounterApp.swift
+//  CashbackCounter
+//
+//  Created by Junhao Huang on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CashbackCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
