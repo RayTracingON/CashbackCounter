b0a6e87225317f2d563e989963045a476701a7a4
Junhao Huang <jh846@students.waikato.ac.nz>
2025-11-24 11:38:59 +0800

    finish take photo to remember bill

---
+++ CashbackCounter/Components/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  CashbackCounter
+//
+//  Created by Junhao Huang on 11/24/25.
+//
+
+import Foundation
---
+++ CashbackCounter/Components/SimpleCamera.swift
@@ -0,0 +1,8 @@
+//
+//  SimpleCamera.swift
+//  CashbackCounter
+//
+//  Created by Junhao Huang on 11/24/25.
+//
+
+import Foundation
